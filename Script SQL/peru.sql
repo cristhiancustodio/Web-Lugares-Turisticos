c1fd9f7a9d3abc10d5aaf3fc76ef165c215ac23d
Cristhian Custodio <76820512+custox06@users.noreply.github.com>
2022-05-01 13:28:43 -0500

    Add files via upload

---
+++ Script SQL/peru.sql
@@ -0,0 +1,32 @@
+drop database peru;
+create database peru;
+use peru;
+create table departamentos(
+	id_departamento int auto_increment primary key not null,
+    nombre_region varchar(50) not null
+);
+
+create table sitios(
+	id int not null primary key auto_increment,
+	nombre varchar(50),
+    id_departamento int,
+    constraint fk_id_departamento
+    foreign key (id_departamento) references departamentos(id_departamento)
+    on delete cascade
+    on update cascade
+);
+
+
+select * from sitios;
+
+
+
+
+
+
+
+
+
+
+
+
